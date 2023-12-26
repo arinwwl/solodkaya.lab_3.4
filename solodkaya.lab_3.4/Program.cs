d8afae9e8b140d791fea301b5dd74e40b05b6bc7
Arina <isp_2_1@mail.ru>
2023-12-26 17:06:48 +0200

    Добавьте файлы проекта.

---
+++ solodkaya.lab_3.4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("|    x   |   y    |");
+for (double x = -0.5; x <= 3; x += 0.2)
+{
+    double y;
+    if (x < 0) y = Math.Exp(x);
+    else if (x >= 0 && x <= 4.5) y = 1-Math.Log(Math.Abs(1+x*x));
+    else y = 2*x;
+    Console.WriteLine($"|{x,8:F2}|{y,8:F2}|");
+}
+
